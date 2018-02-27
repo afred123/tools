2bbb58b325229a0c239ae04d3e9e35ec9c5e10c6
afred123 <1020066573@qq.com>
2018-02-27 20:18:36 +0800

    env.sh modify, add apt source

--- ubuntu-env/env.sh
+++ ubuntu-env/env.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+res=$(lsb_release -a)
+echo $res
+xen='xenial'
+if [[ $res == *$xen* ]]
+then
+   echo "ubuntu16.04"
+   mv /etc/apt/sources.list /etc/apt/sources.list.back16.04
+   mv ubuntu16.04.source /etc/apt/sources.list
+else
+   echo "ubuntu14.04"
+   mv /etc/apt/sources.list /etc/apt/sources.list.back14.04
+   mv ubuntu14.04.source /etc/apt/sources.list
+fi
+apt-get update
